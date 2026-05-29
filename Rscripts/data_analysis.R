4a79d3359d72fa5b44b9a3d82e783e7dfb79e36f
JuanjOvalle <juanjovalle.bio@outlook.com>
2026-05-29 10:59:54 -0500

    Content table and reorganization

--- Rscripts/data_analysis.R
+++ Rscripts/data_analysis.R
@@ -5,16 +5,33 @@
 #                             Version 0.4 - 5/26/2026                          #
 #                                                                              #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Contents
+# 1. Data uploading, phyloseq and rarefaction
+#   1.1 Subset creation
+# 2. Alpha diversity analysis
+#   2.1 Control vs. Pesticides
+#   2.2 Control vs. Flavonoids
+#   2.3 Pesticides vs. Pesticides+Flavonoids
+# 3. Beta diversity analysis
+#   3.1 Bray curtis distances
+#   3.2 PERMANOVA
+# 4. DeSeq, relative abundance analysis
+# 5. Graphs 
+#   5.1 Alpha diversity boxplot
+#   5.2 NMDS
+#   5.3 Relative abundance 
 
 # Libraries
 require("ggplot2")  
 require("BiocManager")
 require("dplyr")
 require("vegan")
-BiocManager::install("phyloseq")
 require("phyloseq")
 require("tidyverse")
+require("pheatmap")
+require("ggsignif")
 
+# 1. Data uploading, processing and rarefaction~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 # Data uploading
 nochim <- read.csv("C:/Users/juanj/Documents/Abejas-Microbiota/Abejas-Microbiota/Analisis/seqtab_nochim_transp_1.csv", row.names = 1, header = TRUE)
@@ -44,10 +61,123 @@ table(tax_table(ps)[, "Phylum"])
 # Rarefaction
 ps_rare <- rarefy_even_depth(ps, rngseed = 123)
 
+# 1.1 Subset creation from the main phyloseq
+
+# Control and all individual molecules
+ps_uni <- subset_samples(ps_rare, 
+                         Treatment %in% c("Control_(N)", "Rutina", "Imidacloprid", "Kaempferol", 
+                                          "p-Coumaric_acid", "Fipronil", "Sugar", "Deltametrina", "Glifosato"))
+
+# Control and all individual pesticides
+ps_pest <- subset_samples(ps_rare, 
+                          Treatment %in% c("Control_(N)", "Imidaclorid", "Fipronil", "Sugar", "Deltametrina", "Glifosato"))
+
+# Control and all individual flavonoids
+ps_phyto <- subset_samples(ps_rare, 
+                           Treatment %in% c("Control_(N)", "Rutina", "Kaempferol", 
+                                            "p-Coumaric_acid", "Sugar"))
+
+# to be continued...
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+# 2. Alpha diversity analysis~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+# Alpha diversity for all treatments
+alpha_div_all  <- estimate_richness(ps_rare, 
+                               measures = c("Observed", 
+                                            "Shannon", 
+                                            "Simpson",
+                                            "Chao1"))
+
+alpha_div_all$Treatment <- as.character(metadata$Treatment)
+alpha_div_all 
+
+# Kruskall-wallis test for all treatments, non significant for any test
+kruskal.test(Shannon ~ Treatment, data = alpha_div_all)
+kruskal.test(Observed ~ Treatment, data = alpha_div_all)
+kruskal.test(Chao1 ~ Treatment, data = alpha_div_all)
+
+# Alpha diversity for each individual molecule
+alpha_div_uni  <- estimate_richness(ps_uni, 
+                                    measures = c("Observed", 
+                                                 "Shannon", 
+                                                 "Simpson",
+                                                 "Chao1"))
+alpha_div_uni$Treatment <- as.character(sample_data(ps_uni)$Treatment)
+alpha_div_uni
+
+# Kruskall-wallis test for each individual molecule, non significant ~
+kruskal.test(Shannon ~ Treatment, data = alpha_div_uni)
+kruskal.test(Observed ~ Treatment, data = alpha_div_uni)
+kruskal.test(Chao1 ~ Treatment, data = alpha_div_uni)
+
+# Alpha diversity for each individual pesticide
+alpha_div_pest  <- estimate_richness(ps_pest, 
+                                    measures = c("Observed", 
+                                                 "Shannon", 
+                                                 "Simpson",
+                                                 "Chao1"))
+alpha_div_pest$Treatment <- as.character(sample_data(ps_pest)$Treatment)
+alpha_div_pest
+
+# Kruskall-wallis test for each individual pesticide, not significant for any treatment
+kruskal.test(Shannon ~ Treatment, data = alpha_div_pest)
+kruskal.test(Observed ~ Treatment, data = alpha_div_pest)
+kruskal.test(Chao1 ~ Treatment, data = alpha_div_pest)
+
+# Alpha diversity for each individual phytochemical
+alpha_div_phyto  <- estimate_richness(ps_phyto, 
+                                     measures = c("Observed", 
+                                                  "Shannon", 
+                                                  "Simpson",
+                                                  "Chao1"))
+alpha_div_phyto$Treatment <- as.character(sample_data(ps_phyto)$Treatment)
+alpha_div_phyto
+
+# Kruskall-wallis test for each individual phytoicide, not significant for any treatment
+kruskal.test(Shannon ~ Treatment, data = alpha_div_phyto)
+kruskal.test(Observed ~ Treatment, data = alpha_div_phyto)
+kruskal.test(Chao1 ~ Treatment, data = alpha_div_phyto)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+# 3. Beta diversity analysis~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+# 3.1 Bray curtis distances
+
+# Calculate distances between individuals, Bray Curtis method
+bray_all <- phyloseq::distance(ps_rare, method = "bray")
+
+# Average distance per treatments
+distances_all <- meandist(bray_all, sample_data(ps_rare)$Treatment)
+
+View(distances_all)
+
+# Calculate distances between individual molecules, Bray Curtis method
+bray_uni <- phyloseq::distance(ps_uni, method = "bray")
+
+# Average distance per treatments
+distances_uni <- meandist(bray_uni, sample_data(ps_uni)$Treatment)
+
+View(distances_uni)
+
+# 3.2 PERMANOVA
+
+# For all treatments
+permanova_all <- adonis2(bray_all ~ Treatment,
+                         data = as.data.frame(metadata_ordered),
+                         permutations = 999)
+
+# For all individual molecules
+permanova_uni <- adonis2(bray_uni ~ sample_data(ps_uni)$Treatment,
+                         data = as.data.frame(metadata),
+                         permutations = 999)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
 # Bray-Curtis NMDS
 ord_nmds <- ordinate(ps_rare, method = "NMDS", distance = "bray")
 
-# Stress for bray-curtis distance, less than 0.2
+# Stress for NMDS, less than 0.2
 ord_nmds$stress
 
 #Graph NMDS for all treatments simultaneously
@@ -68,10 +198,10 @@ ggsave("Figures/graph_nmds_all.jpg", plot = graph_nmds_all)
 
 bray_dist <- phyloseq::distance(ps_rare, method = "bray")
 
-permanova <- adonis2(bray_dist ~ Treatment,
+permanova_all <- adonis2(bray_dist ~ Treatment,
                      data = as.data.frame(metadata),
                      permutations = 999)
-permanova #Not significant when evaluating everything at once
+permanova_all #Not significant when evaluating everything at once
 
 sample_data(ps_rare)$Treated <- ifelse(
   sample_data(ps_rare)$Treatment == "Control_(N)", 
@@ -102,7 +232,7 @@ bray_uni <- phyloseq::distance(ps_uni, method = "bray")
 nmds_points_uni <- as.data.frame(ord_nmds_uni$points)
 nmds_points_uni$Treatment <- sample_data(ps_uni)$Treatment
 
-adonis2(bray_uni ~ Treatment,
+permanova_uni <- adonis2(bray_uni ~ Treatment,
         data = data.frame(sample_data(ps_uni)),
         permutations = 999)
 
@@ -114,7 +244,25 @@ graph_nmds_uni <- ggplot(nmds_points_uni, aes(x = MDS1, y = MDS2, color = Treatm
        subtitle = paste("Stress =", round(ord_nmds_uni$stress, 3))) +
   theme_bw()
 
-#Only pesticides
+#Bray curtis distance represented
+bray_matrix <- as.matrix(bray_uni)
+
+# Agregar tratamiento a la matriz
+treatments <- data.frame(sample_data(ps_uni))$Treatment
+
+# Distancia promedio entre cada par de tratamientos
+distances_all <- meandist(bray_uni, treatments)
+
+View(distances_all)
+
+dist_df <- as.data.frame(as.matrix(distances_all))
+
+pheatmap::pheatmap(distances_all,
+                   display_numbers = TRUE,
+                   number_format = "%.2f",
+                   main = "All treatments average Bray curtis distance")
+
+#All pesticides
 
 ps_pest <- subset_samples(ps_rare, 
                          Treatment %in% c("Control_(N)", "Imidaclorid", "Fipronil", "Sugar", "Deltametrina", "Glifosato"))
@@ -139,7 +287,26 @@ graph_nmds_pest <- ggplot(nmds_points_pest, aes(x = MDS1, y = MDS2, color = Trea
 
 graph_nmds_pest
 
-#Only flavonoids
+#Individual pesticides represented
+
+bray_matrix_pest <- as.matrix(bray_pest)
+
+# Agregar tratamiento a la matriz
+treatments <- data.frame(sample_data(ps_pest))$Treatment
+
+# Distancia promedio entre cada par de tratamientos
+distances_pest <- meandist(bray_pest, treatments)
+
+View(distances_pest)
+
+dist_df_pest <- as.data.frame(as.matrix(distances_pest))
+
+pheatmap::pheatmap(dist_df_pest,
+                   display_numbers = TRUE,
+                   number_format = "%.2f",
+                   main = "Distancia Bray-Curtis entre pesticidas")
+
+#All flavonoids
 
 ps_phyto <- subset_samples(ps_rare, 
                          Treatment %in% c("Control_(N)", "Rutina", "Kaempferol", 
@@ -188,3 +355,57 @@ bray_mol1 <- phyloseq::distance(ps_mol1, method = "bray")
 adonis2(bray_mol1 ~ Treatment,
         data = data.frame(sample_data(ps_mol1)),
         permutations = 999)
+
+# Filtrar taxa con abundancia relativa promedio > 1%
+ps_fil <- filter_taxa(ps_uni,
+                      function(x) mean(x/sum(x)) > 0.01 & 
+                        sum(x > 0) > 0.2 * nsamples(ps_uni),
+                      TRUE)
+
+ps_phylum <- tax_glom(ps_fil, taxrank = "Phylum")
+plot_bar(ps_phylum, fill = "Phylum", x = "Treatment") +
+  theme_bw()
+
+# A nivel de Género
+ps_genus <- tax_glom(ps_fil, taxrank = "Genus")
+plot_bar(ps_genus, fill = "Genus", x = "Treatment") +
+  theme_bw()
+
+
+# Calcular métricas de diversidad alfa
+alpha_div <- estimate_richness(ps_uni, 
+                               measures = c("Observed", 
+                                            "Shannon", 
+                                            "Simpson",
+                                            "Chao1"))
+
+# Agregar metadata
+alpha_div$Treatment <- sample_data(ps_uni)$Treatment
+alpha_div$Sample <- rownames(alpha_div)
+
+View(alpha_div)
+
+alpha_div %>%
+  pivot_longer(cols = c(Observed),
+               names_to = "metric",
+               values_to = "value") %>%
+  ggplot(aes(x = Treatment, y = value, fill = Treatment)) +
+  geom_boxplot(alpha = 0.7) +
+  geom_signif(
+    comparisons = list(
+      c("Control_(N)", "Rutina"), c("Rutina", "Deltametrina")
+    ),map_signif_level = TRUE) +
+  geom_jitter(width = 0.2, size = 1.5) +
+  facet_wrap(~ metric, scales = "free_y") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "none")
+
+pairwise_adonis(
+  ps_uni,
+  Treatment,
+  metadata = NULL,
+  perm_design = NULL,
+  p.adjust.method = "bonferroni",
+  perm = 999
+)
